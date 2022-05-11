8a38873136549988b889ef03a2cae95ab7d08d97
teerenzo <renzahoemmanuel8@gmail.com>
2022-05-11 10:06:38 +0200

    add screen

--- lib/screens/multiple_sign_in_options.dart
+++ lib/screens/multiple_sign_in_options.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:panacea/screens/fingerprint.dart';
 import 'package:panacea/screens/no/email_password/sign-in-email.dart';
 import 'package:panacea/screens/no/phone_password/sign-in-with-phone-number.dart';
 import 'package:panacea/widgets/app_large_text.dart';
@@ -21,30 +20,42 @@ class MultipleSignInOptions extends StatelessWidget {
               Expanded(
                 child: Container(
                   padding: const EdgeInsets.only(top: 54),
-                  child:
-                  Center(child: AppLargeText(text: 'PANACEA',)),
+                  child: Center(
+                      child: AppLargeText(
+                    text: 'PANACEA',
+                  )),
                 ),
               ),
-              SizedBox(height: 60,),
-              Container(
-                child: Center(child: AppText(text: 'Anonymous access to\n Healthcare services',)),
+              SizedBox(
+                height: 60,
               ),
-              SizedBox(height: 50,),
               Container(
-                  child: Center(child: Text('PANACEA IS ANONYMOUS'))
+                child: Center(
+                    child: AppText(
+                  text: 'Anonymous access to\n Healthcare services',
+                )),
               ),
-              SizedBox(height: 20,),
+              SizedBox(
+                height: 50,
+              ),
+              Container(child: Center(child: Text('PANACEA IS ANONYMOUS'))),
+              SizedBox(
+                height: 20,
+              ),
               Container(
-                child: TextButton(onPressed: (){
-                  Navigator.pushNamed(context, Fingerprint.id);
-                },
-                    child: Center(child: Text('Would you like to remain anonymous?'))),
+                child: TextButton(
+                    onPressed: () {},
+                    child: Center(
+                        child: Text('Would you like to remain anonymous?'))),
               ),
               SizedBox(
                 height: 20,
               ),
-              Center(child: Text('You can also Sign In with email or\n telephone number',
-                textAlign: TextAlign.center,),
+              Center(
+                child: Text(
+                  'You can also Sign In with email or\n telephone number',
+                  textAlign: TextAlign.center,
+                ),
               ),
               SizedBox(
                 height: 140,
@@ -52,24 +63,30 @@ class MultipleSignInOptions extends StatelessWidget {
               Container(
                 child: Row(
                   children: [
-                    TextButton(onPressed: (){
-                      Navigator.pushNamed(context, SignInEmail.id);
-                    },
-                      child: Text('Sign in with email'),),
-                    SizedBox(width: SizeConfig.screenWidth! * .012,),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, SignInEmail.id);
+                      },
+                      child: Text('Sign in with email'),
+                    ),
+                    SizedBox(
+                      width: SizeConfig.screenWidth! * .012,
+                    ),
                     Padding(
                       padding: const EdgeInsets.only(left: 20),
-                      child: TextButton(onPressed: (){
-                        Navigator.pushNamed(context, SignInWithPhoneNumber.id);
-                      },
-                        child: Text('Telephone number'),),
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(
+                              context, SignInWithPhoneNumber.id);
+                        },
+                        child: Text('Telephone number'),
+                      ),
                     ),
                   ],
                 ),
               )
             ],
           ),
-        )
-    );
+        ));
   }
 }
